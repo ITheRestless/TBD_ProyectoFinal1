93577ec0fae43e724f7756021879d1641c2567e3
Jesus Alberto Diaz de Leon <chuylm1@live.com.mx>
2019-12-15 17:18:46 -0600

    Hola
    solo hice pull

--- src/tbd_proyectofinal/Fondos.java
+++ src/tbd_proyectofinal/Fondos.java
@@ -27,17 +27,76 @@ public class Fondos extends javax.swing.JFrame {
     // <editor-fold defaultstate="collapsed" desc="Generated Code">//GEN-BEGIN:initComponents
     private void initComponents() {
 
+        buttonGroup1 = new javax.swing.ButtonGroup();
+        jLabel1 = new javax.swing.JLabel();
+        rB5 = new javax.swing.JRadioButton();
+        rB10 = new javax.swing.JRadioButton();
+        rB15 = new javax.swing.JRadioButton();
+        jButton1 = new javax.swing.JButton();
+        jButton2 = new javax.swing.JButton();
+
         setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
+
+        jLabel1.setFont(new java.awt.Font("Bahnschrift", 0, 11)); // NOI18N
+        jLabel1.setText("FONDOS");
+
+        buttonGroup1.add(rB5);
+        rB5.setFont(new java.awt.Font("Bahnschrift", 0, 11)); // NOI18N
+        rB5.setText("5 Fondos");
+
+        buttonGroup1.add(rB10);
+        rB10.setFont(new java.awt.Font("Bahnschrift", 0, 11)); // NOI18N
+        rB10.setText("10 Fondos");
+
+        buttonGroup1.add(rB15);
+        rB15.setFont(new java.awt.Font("Bahnschrift", 0, 11)); // NOI18N
+        rB15.setText("15 Fondos");
+
+        jButton1.setFont(new java.awt.Font("Bahnschrift", 0, 11)); // NOI18N
+        jButton1.setText("Añadir fondo");
+
+        jButton2.setFont(new java.awt.Font("Bahnschrift", 0, 11)); // NOI18N
+        jButton2.setText("Regresar a menu");
 
         javax.swing.GroupLayout layout = new javax.swing.GroupLayout(getContentPane());
         getContentPane().setLayout(layout);
         layout.setHorizontalGroup(
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addGap(0, 400, Short.MAX_VALUE)
+            .addGroup(layout.createSequentialGroup()
+                .addGap(56, 56, 56)
+                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                    .addGroup(layout.createSequentialGroup()
+                        .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                            .addComponent(rB5)
+                            .addComponent(rB10)
+                            .addComponent(rB15))
+                        .addContainerGap(javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE))
+                    .addGroup(layout.createSequentialGroup()
+                        .addComponent(jButton1)
+                        .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED, 144, Short.MAX_VALUE)
+                        .addComponent(jButton2)
+                        .addGap(76, 76, 76))))
+            .addGroup(layout.createSequentialGroup()
+                .addGap(205, 205, 205)
+                .addComponent(jLabel1)
+                .addContainerGap(javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE))
         );
         layout.setVerticalGroup(
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addGap(0, 300, Short.MAX_VALUE)
+            .addGroup(layout.createSequentialGroup()
+                .addGap(38, 38, 38)
+                .addComponent(jLabel1)
+                .addGap(28, 28, 28)
+                .addComponent(rB5, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+                .addGap(33, 33, 33)
+                .addComponent(rB10, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+                .addGap(33, 33, 33)
+                .addComponent(rB15, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+                .addGap(27, 27, 27)
+                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
+                    .addComponent(jButton1)
+                    .addComponent(jButton2))
+                .addGap(35, 35, 35))
         );
 
         pack();
@@ -79,5 +138,12 @@ public class Fondos extends javax.swing.JFrame {
     }
 
     // Variables declaration - do not modify//GEN-BEGIN:variables
+    private javax.swing.ButtonGroup buttonGroup1;
+    private javax.swing.JButton jButton1;
+    private javax.swing.JButton jButton2;
+    private javax.swing.JLabel jLabel1;
+    private javax.swing.JRadioButton rB10;
+    private javax.swing.JRadioButton rB15;
+    private javax.swing.JRadioButton rB5;
     // End of variables declaration//GEN-END:variables
 }
